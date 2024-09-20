edb7eea79af12309afb37e956956609572fde394
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-09-20 18:17:17 -0500

    Update block_cajaherramientas.php

--- block_cajaherramientas.php
+++ block_cajaherramientas.php
@@ -37,7 +37,7 @@ class block_cajaherramientas extends block_base
         $subtitle = isset($config->subtitle) ? format_string($config->subtitle) : 'Mejora tus habilidades con los mejores cursos online';
 
         // Comienza el contenedor principal.
-        $output .= html_writer::start_div('caja-herramientas');
+        $output .= html_writer::start_div('block_cajaherramientas_container');
 
         // Imagen de fondo.
         $bgstyle = '';
@@ -60,20 +60,21 @@ class block_cajaherramientas extends block_base
 
         // Aplicar estilo de fondo si hay una imagen.
         if ($bgstyle) {
-            $output = str_replace('class="caja-herramientas"', 'class="caja-herramientas" style="' . $bgstyle . '"', $output);
+            $output = str_replace('class="block_cajaherramientas_container"', 'class="block_cajaherramientas_container" style="' . $bgstyle . '"', $output);
         }
 
         // Encabezado del bloque.
-        $output .= html_writer::start_div('caja-header');
-        $output .= html_writer::tag('h2', $title, array('class' => 'caja-title'));
-        $output .= html_writer::tag('p', $subtitle, array('class' => 'caja-subtitle'));
-        $output .= html_writer::end_div(); // Fin de caja-header.
+        $output .= html_writer::start_div('block_cajaherramientas_header');
+        $output .= html_writer::tag('h2', $title, array('class' => 'block_cajaherramientas_title'));
+        $output .= html_writer::tag('p', $subtitle, array('class' => 'block_cajaherramientas_subtitle'));
+        $output .= html_writer::end_div(); // Fin de block_cajaherramientas_header.
 
         // Contenedor de tarjetas.
-        $output .= html_writer::start_div('cards-container');
+        $output .= html_writer::start_div('block_cajaherramientas_cards_container');
 
         // Cursos destacados.
-        $coursecount = isset($config->coursecount) ? $config->coursecount : 3;
+        $coursecount = isset($config->coursecount) ? $config->coursecount : 1;
+        $coursecount = max(1, min(3, $coursecount)); // Asegura que el número de cursos esté entre 1 y 3
         for ($i = 1; $i <= $coursecount; $i++) {
             $coursetitle = isset($config->{"coursename$i"}) ? format_string($config->{"coursename$i"}) : "Curso $i";
             $coursetype = isset($config->{"coursetype$i"}) ? format_string($config->{"coursetype$i"}) : "Tipo de curso";
@@ -82,37 +83,36 @@ class block_cajaherramientas extends block_base
             $courseurl = isset($config->{"courseurl$i"}) ? $config->{"courseurl$i"} : '#';
 
             // Comienza la tarjeta.
-            $output .= html_writer::start_div('card');
+            $output .= html_writer::start_div('block_cajaherramientas_card');
 
             // Encabezado de la tarjeta.
-            $output .= html_writer::start_div('card-header');
-            $output .= html_writer::div($coursetype, 'cocoon-card-header');
-            $output .= html_writer::end_div(); // Fin de card-header.
+            $output .= html_writer::start_div('block_cajaherramientas_card_header');
+            $output .= html_writer::div($coursetype, 'block_cajaherramientas_card_header_text');
+            $output .= html_writer::end_div(); // Fin de block_cajaherramientas_card_header.
 
             // Cuerpo de la tarjeta.
-            $output .= html_writer::start_div('card-body');
-            $output .= html_writer::tag('h3', $coursetitle, array('class' => 'card-title'));
-            $output .= html_writer::tag('p', $coursetext1, array('class' => 'card-text'));
-            $output .= html_writer::tag('p', $coursetext2, array('class' => 'card-text'));
-            $output .= html_writer::end_div(); // Fin de card-body.
+            $output .= html_writer::start_div('block_cajaherramientas_card_body');
+            $output .= html_writer::tag('h3', $coursetitle, array('class' => 'block_cajaherramientas_card_title'));
+            $output .= html_writer::tag('p', $coursetext1, array('class' => 'block_cajaherramientas_card_text'));
+            $output .= html_writer::tag('p', $coursetext2, array('class' => 'block_cajaherramientas_card_text'));
+            $output .= html_writer::end_div(); // Fin de block_cajaherramientas_card_body.
 
             // Botón "Ingresar".
-            $output .= html_writer::link($courseurl, 'Ingresar', array('class' => 'card-button'));
+            $output .= html_writer::link($courseurl, 'Ingresar', array('class' => 'block_cajaherramientas_card_button'));
 
-            $output .= html_writer::end_div(); // Fin de card.
+            $output .= html_writer::end_div(); // Fin de block_cajaherramientas_card.
         }
 
-        $output .= html_writer::end_div(); // Fin de cards-container.
+        $output .= html_writer::end_div(); // Fin de block_cajaherramientas_cards_container.
 
         // Botón "Ver el portafolio".
         $portfolio_url = isset($config->portfolio_url) ? $config->portfolio_url : '#';
-        $output .= html_writer::link($portfolio_url, 'Ver el portafolio', array('class' => 'portfolio-button'));
+        $output .= html_writer::link($portfolio_url, 'Ver el portafolio', array('class' => 'block_cajaherramientas_portfolio_button'));
 
-        $output .= html_writer::end_div(); // Fin de caja-herramientas.
+        $output .= html_writer::end_div(); // Fin de block_cajaherramientas_container.
 
         return $output;
     }
-
 
     function applicable_formats()
     {
@@ -132,13 +132,14 @@ class block_cajaherramientas extends block_base
                 'block_cajaherramientas',
                 'backgroundimage',
                 0,
-                array('subdirs' => false)
+                array('subdirs' => 0, 'maxbytes' => 1048576, 'maxfiles' => 1, 'accepted_types' => 'web_image')
             );
             $data->backgroundimage = '';
         }
 
         parent::instance_config_save($data, $nolongerused);
     }
+
     public function html_attributes()
     {
         global $CFG;
