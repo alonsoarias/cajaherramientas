155a640ed04833c4c36a6b03743acfe620cbf993
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-09-20 17:19:11 -0500

    Update block_cajaherramientas.php

--- lang/es/block_cajaherramientas.php
+++ lang/es/block_cajaherramientas.php
@@ -1,14 +1,36 @@
 <?php
 $string['pluginname'] = 'Caja de Herramientas';
-$string['cajaherramientas:addinstance'] = 'Agregar un nuevo bloque de Caja de Herramientas';
-$string['cajaherramientas:myaddinstance'] = 'Agregar un nuevo bloque de Caja de Herramientas al panel';
+$string['cajaherramientas'] = 'Caja de Herramientas';
+$string['cajaherramientas:addinstance'] = 'Añadir un nuevo bloque de Caja de Herramientas';
+$string['cajaherramientas:myaddinstance'] = 'Añadir un nuevo bloque de Caja de Herramientas a Mi Moodle';
+
+// Cadenas para la configuración del bloque
 $string['title'] = 'Título';
 $string['subtitle'] = 'Subtítulo';
-$string['coursename'] = 'Nombre del curso';
+$string['backgroundimage'] = 'Imagen de fondo';
+$string['coursecount'] = 'Número de cursos';
+$string['course'] = 'Curso';
+$string['coursename'] = 'Título del curso';
 $string['coursetype'] = 'Tipo de curso';
+$string['coursetext1'] = 'Texto adicional 1';
+$string['coursetext2'] = 'Texto adicional 2';
 $string['teachername'] = 'Nombre del docente';
 $string['courseurl'] = 'URL del curso';
-$string['backgroundimage'] = 'Imagen de fondo';
 $string['portfolio_url'] = 'URL del portafolio';
+
+// Valores predeterminados
+$string['defaulttitle'] = 'Caja de Herramientas';
+$string['defaultsubtitle'] = 'Mejora tus habilidades con los mejores cursos online';
+$string['defaultcoursename'] = 'Curso {$a}';
+$string['defaultcoursetype'] = 'Cursos de extensión';
+$string['defaultcoursetext1'] = 'Electiva/Seminario/Cátedra/Práctica';
+$string['defaultcoursetext2'] = 'Docente: Nombre N. Apellido A';
+$string['defaultteachername'] = 'Docente: Nombre N. Apellido A';
+
+// Etiquetas de botones
 $string['enter'] = 'Ingresar';
 $string['viewportfolio'] = 'Ver el portafolio';
+
+// Mensajes de error
+$string['missingbackground'] = 'Por favor, sube una imagen de fondo';
+$string['invalidcoursecount'] = 'El número de cursos debe ser entre 1 y 3';
