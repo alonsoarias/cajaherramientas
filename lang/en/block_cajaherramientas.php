161a477484e9d9024d399e87a4164a05e88b7fc6
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-09-20 18:17:21 -0500

    Create block_cajaherramientas.php

---
+++ lang/en/block_cajaherramientas.php
@@ -0,0 +1,37 @@
+<?php
+// English language strings for the Toolbox (Caja de Herramientas) block
+$string['pluginname'] = 'Toolbox';
+$string['cajaherramientas'] = 'Toolbox';
+$string['cajaherramientas:addinstance'] = 'Add a new Toolbox block';
+$string['cajaherramientas:myaddinstance'] = 'Add a new Toolbox block to My Moodle';
+
+// Strings for the block settings
+$string['title'] = 'Title';
+$string['subtitle'] = 'Subtitle';
+$string['backgroundimage'] = 'Background Image';
+$string['coursecount'] = 'Number of Courses';
+$string['course'] = 'Course';
+$string['coursename'] = 'Course Title';
+$string['coursetype'] = 'Course Type';
+$string['coursetext1'] = 'Additional Text 1';
+$string['coursetext2'] = 'Additional Text 2';
+$string['teachername'] = 'Teacher Name';
+$string['courseurl'] = 'Course URL';
+$string['portfolio_url'] = 'Portfolio URL';
+
+// Default values
+$string['defaulttitle'] = 'Toolbox';
+$string['defaultsubtitle'] = 'Improve your skills with the best online courses';
+$string['defaultcoursename'] = 'Course {$a}';
+$string['defaultcoursetype'] = 'Extension Courses';
+$string['defaultcoursetext1'] = 'Elective/Seminar/Lecture/Practice';
+$string['defaultcoursetext2'] = 'Teacher: First Name Last Name';
+$string['defaultteachername'] = 'Teacher: First Name Last Name';
+
+// Button labels
+$string['enter'] = 'Enter';
+$string['viewportfolio'] = 'View Portfolio';
+
+// Error messages
+$string['missingbackground'] = 'Please upload a background image';
+$string['invalidcoursecount'] = 'The number of courses must be between 1 and 3';
