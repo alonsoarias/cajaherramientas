6b83d2d1c98e5af30452bc86b7b997fd78fd3017
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-09-20 17:19:09 -0500

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -1,46 +1,75 @@
 <?php
-class block_cajaherramientas_edit_form extends block_edit_form {
+defined('MOODLE_INTERNAL') || die();
 
-    protected function specific_definition($mform) {
+class block_cajaherramientas_edit_form extends block_edit_form
+{
 
-        // Sección para el título y subtítulo
-        $mform->addElement('header', 'configheader', get_string('pluginname', 'block_cajaherramientas'));
+    protected function specific_definition($mform)
+    {
+        // Sección para el título y subtítulo.
+        $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
 
+        // Campo para el título.
         $mform->addElement('text', 'config_title', get_string('title', 'block_cajaherramientas'));
+        $mform->setDefault('config_title', 'Caja de Herramientas');
         $mform->setType('config_title', PARAM_TEXT);
 
+        // Campo para el subtítulo.
         $mform->addElement('text', 'config_subtitle', get_string('subtitle', 'block_cajaherramientas'));
+        $mform->setDefault('config_subtitle', 'Mejora tus habilidades con los mejores cursos online');
         $mform->setType('config_subtitle', PARAM_TEXT);
 
-        // Campos para cada curso
-        for ($i = 1; $i <= 3; $i++) {
-            $mform->addElement('header', "courseheader$i", "Curso $i");
+        // Selector de archivo para la imagen de fondo.
+        $mform->addElement('filemanager', 'config_backgroundimage', get_string('backgroundimage', 'block_cajaherramientas'), null, array(
+            'maxbytes' => 2097152,
+            'accepted_types' => array('.jpg', '.png', '.gif'),
+            'maxfiles' => 1
+        ));
+        $mform->setType('config_backgroundimage', PARAM_INT);
 
+        // Control de la cantidad de cursos.
+        $coursecount_options = array(
+            1 => '1',
+            2 => '2',
+            3 => '3'
+        );
+        $mform->addElement('select', 'config_coursecount', get_string('coursecount', 'block_cajaherramientas'), $coursecount_options);
+        $mform->setDefault('config_coursecount', 3);
+        $mform->setType('config_coursecount', PARAM_INT);
+
+        // URL del portafolio.
+        $mform->addElement('text', 'config_portfolio_url', get_string('portfolio_url', 'block_cajaherramientas'));
+        $mform->setType('config_portfolio_url', PARAM_URL);
+
+        // Campos para cada curso.
+        for ($i = 1; $i <= 3; $i++) {
+            $mform->addElement('header', "courseheader$i", get_string('course', 'block_cajaherramientas') . " $i");
+
+            // Título del curso.
             $mform->addElement('text', "config_coursename$i", get_string('coursename', 'block_cajaherramientas'));
             $mform->setType("config_coursename$i", PARAM_TEXT);
 
+            // Tipo de curso.
             $mform->addElement('text', "config_coursetype$i", get_string('coursetype', 'block_cajaherramientas'));
             $mform->setType("config_coursetype$i", PARAM_TEXT);
 
-            $mform->addElement('text', "config_teachername$i", get_string('teachername', 'block_cajaherramientas'));
-            $mform->setType("config_teachername$i", PARAM_TEXT);
+            // Texto adicional 1.
+            $mform->addElement('text', "config_coursetext1_$i", get_string('coursetext1', 'block_cajaherramientas'));
+            $mform->setType("config_coursetext1_$i", PARAM_TEXT);
 
+            // Texto adicional 2.
+            $mform->addElement('text', "config_coursetext2_$i", get_string('coursetext2', 'block_cajaherramientas'));
+            $mform->setType("config_coursetext2_$i", PARAM_TEXT);
+
+            // URL del curso.
             $mform->addElement('text', "config_courseurl$i", get_string('courseurl', 'block_cajaherramientas'));
             $mform->setType("config_courseurl$i", PARAM_URL);
         }
-
-        // Selector de archivo para la imagen de fondo
-        $mform->addElement('filemanager', 'config_backgroundimage', get_string('backgroundimage', 'block_cajaherramientas'), null, array(
-            'maxfiles' => 1,
-            'accepted_types' => array('image')
-        ));
-
-        // URL del portafolio
-        $mform->addElement('text', 'config_portfolio_url', get_string('portfolio_url', 'block_cajaherramientas'));
-        $mform->setType('config_portfolio_url', PARAM_URL);
     }
 
-    public function set_data($defaults) {
+    public function set_data($defaults)
+    {
+        // Manejar la imagen de fondo.
         if (isset($this->block->config->backgroundimage)) {
             $draftitemid = file_get_submitted_draft_itemid('config_backgroundimage');
             file_prepare_draft_area(
@@ -53,6 +82,7 @@ class block_cajaherramientas_edit_form extends block_edit_form {
             );
             $defaults->config_backgroundimage = $draftitemid;
         }
+
         parent::set_data($defaults);
     }
 }
