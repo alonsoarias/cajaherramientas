fc31d6647926226ef37fe1db3b42dcdef6551c87
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-09-24 17:11:30 -0500

    Update version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_cajaherramientas';
-$plugin->version = 2023100100; // Fecha en formato AAAAMMDDHH
+$plugin->version  = 2024090100;  // Fecha en formato AAAAMMDDHH
 $plugin->requires = 2020061500; // Requiere Moodle 3.9 o superior
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0';
